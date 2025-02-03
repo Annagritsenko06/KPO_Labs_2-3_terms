9136d46b13491eab72c19ff3a47f6dcb8b318155
Annagritsenko06 <153274338+Annagritsenko06@users.noreply.github.com>
2025-02-03 18:50:07 +0300

    Add files via upload

---
+++ SELab17/SELab17/LT.h
@@ -0,0 +1,67 @@
+﻿#pragma once
+#pragma once
+#pragma once
+#include "IT.h"
+#define LEXEMA_FIXSIZE  1			//������������� ������ �������
+#define	LT_MAXSIZE		4096		// ������������ ���������� ����� � ������� ������
+#define	LT_TI_NULLXDX	-1	//��� �������� ������� ���������������				
+#define	LEX_INTEGER		't'			// ������� ��� integer
+#define	LEX_STRING		't'			// ������� ��� string
+#define	LEX_ID			'i'			// ������� ��� ��������������
+#define	LEX_LITERAL		'l'			// ������� ��� ��������
+#define	LEX_FUNCTION	'f'			// ������� ��� function
+#define	LEX_DECLARE		'd'			// ������� ��� declare
+#define	LEX_RETURN		'r'			// ������� ��� return
+#define	LEX_PRINT		'p'			// ������� ��� print
+#define	LEX_STRLEN		'e'			// ������� ��� strlen
+#define	LEX_SEMICOLON	';'			// ������� ��� ;
+#define	LEX_COMMA		','			// ������� ��� ,
+#define	LEX_LEFTBRACE	'{'			// ������� ��� {
+#define	LEX_RIGHTBRACE	'}'			// ������� ��� }
+#define	LEX_LEFTHESIS	'('			// ������� ��� (
+#define	LEX_RIGHTHESIS	')'			// ������� ��� )
+#define	LEX_PLUS		'v'			// ������� ��� +
+#define	LEX_MINUS		'v'			// ������� ��� -
+#define	LEX_STAR		'v'			// ������� ��� *
+#define	LEX_DIRSLASH	'v'			// ������� ��� /
+#define	LEX_EQUAL_SIGN	'='			// ������� ��� =
+
+#define PLUS '+'
+#define MINUS '-'
+#define STAR '*'
+#define DIRSLASH '/'
+
+
+namespace LT									// ������� ������
+{
+	struct Entry								// ������ ������� ������
+	{
+		char lexema;							// �������
+		int sn;									// ����� ������ � �������� ������
+		int idxTI;								// ������ � ������� ��������������� ��� LT_TI_NULLIDX
+		Entry();
+		Entry(const char lex, int str_n, int idxTI);
+	};
+
+	struct LexTable								// �������� ������� ������
+	{
+		int maxsize;							// ������� ������� ������ < LT_MAXSIZE
+		int size;								// ������� ������ ������� ������ < maxsize
+		Entry* table;							//������ ����� ������� ������
+
+
+		Entry GetEntry(								// �������� ������ ������� ������
+			int n									// ����� ���������� ������
+		);
+
+		void PrintLexTable(const wchar_t* in);
+		LexTable();
+	};
+
+	void Add(									// �������� ������ � ������� ������
+		LexTable& lextable,						//��������� ������� ������
+		Entry entry								// ������ ������� ������
+	);
+
+	void Delete(LexTable& lextable);			// ������� ������� ������ (���������� ������)
+};
