f626066a8fab42c6bd322db8172be50d27002979
Annagritsenko06 <153274338+Annagritsenko06@users.noreply.github.com>
2025-02-03 18:56:49 +0300

    Add files via upload

---
+++ 19 лаба/lab_19/SE_asm01c/SE_asm01c.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "C:\\Users\\admin\\Documents\\уник\\3 СЕМ\КПО\\19 лаба\\lab_19\\Debug\\SE_asm01a.lib"
+extern "C"
+{
+    int __stdcall getmin(int*, int);
+    int __stdcall getmax(int*, int);
+}
+
+int main()
+{
+    int arr[10] = { 56, 2, 8, -22, 100, 98, 71, -11, 7, 14 };
+    int min = 0, max = 0, sum = 0;
+    min = getmin(arr, 10);
+    max = getmax(arr, 10);
+    sum = min + max;
+    std::cout << "getmax + getmin = " << sum << std::endl;
+}
